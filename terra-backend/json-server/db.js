0737abd5c21fa04dcb251958d124f4c2f5307052
francescoamato <francamato@gmail.com>
2022-11-18 23:42:41 +0100

    add multi languages cls

--- terra-backend/json-server/db.js
+++ terra-backend/json-server/db.js
@@ -5,18 +5,31 @@ const metadata = JSON.parse(fs.readFileSync("./data/general/metadata.json"));
 
 //Products
 const productsCPA = JSON.parse(fs.readFileSync("./data/classification/clsProductsCPA.json"));
+const productsCPA_it = JSON.parse(fs.readFileSync("./data/classification/clsProductsCPA_it.json"));
+const productsCPA_en = JSON.parse(fs.readFileSync("./data/classification/clsProductsCPA_en.json"));
+
 const productsIntra = JSON.parse(fs.readFileSync("./data/classification/clsProductsGraphIntra.json"));
+const productsIntra_it = JSON.parse(fs.readFileSync("./data/classification/clsProductsGraphIntra_it.json"));
+const productsIntra_en = JSON.parse(fs.readFileSync("./data/classification/clsProductsGraphIntra_en.json"));
+
 const productsExtra = JSON.parse(fs.readFileSync("./data/classification/clsProductsGraphExtraNSTR.json"));
-//const products = JSON.parse(fs.readFileSync("./data/classification/clsProducts.json"));
-//const productplus = JSON.parse(fs.readFileSync("./data/classification/clsProductplus.json"));
-//const product3s = JSON.parse(fs.readFileSync("./data/classification/clsProducts3.json"));
-//const becs = JSON.parse(fs.readFileSync("./data/classification/clsBec.json"));
+const productsExtra_it = JSON.parse(fs.readFileSync("./data/classification/clsProductsGraphExtraNSTR_it.json"));
+const productsExtra_en = JSON.parse(fs.readFileSync("./data/classification/clsProductsGraphExtraNSTR_en.json"));
+
 
 const transports = JSON.parse(fs.readFileSync("./data/classification/clsTransport.json"));
+const transports_it = JSON.parse(fs.readFileSync("./data/classification/clsTransport_it.json"));
+const transports_en = JSON.parse(fs.readFileSync("./data/classification/clsTransport_en.json"));
+
 const partners = JSON.parse(fs.readFileSync("./data/classification/clsPartners.json"));
+const partners_it = JSON.parse(fs.readFileSync("./data/classification/clsPartners_it.json"));
+const partners_en = JSON.parse(fs.readFileSync("./data/classification/clsPartners_en.json"));
 
 //Map
 const countries = JSON.parse(fs.readFileSync("./data/general/countries.json"));
+const countries_it = JSON.parse(fs.readFileSync("./data/general/countries_it.json"));
+const countries_en = JSON.parse(fs.readFileSync("./data/general/countries_en.json"));
+
 const countriesBorders = JSON.parse(fs.readFileSync("./data/map/countriesBorders.json"));
 //structural info
 const ieinfo = JSON.parse(fs.readFileSync("./data/map/ieinfo.json"));
@@ -37,12 +50,24 @@ module.exports = () => ({
   metadata,
   //classifications
   transports,
+  transports_it,
+  transports_en,
   partners,
+  partners_it,
+  partners_en,
   productsCPA,
+  productsCPA_it,
+  productsCPA_en,
   productsIntra,
+  productsIntra_it,
+  productsIntra_en,
   productsExtra,
+  productsExtra_it,
+  productsExtra_en,
   //map
   countries,
+  countries_it,
+  countries_en,
   countriesBorders,  
   ieinfo,
   importseries,
