b750c61f2a64388b0bfd80db2fa192bc4a0ee981
papizzo <papizzo@istat.it>
2023-12-29 13:01:02 +0100

    fix length on tr news ok

--- terra-frontend/src/views/graph/GraphTable.vue
+++ terra-frontend/src/views/graph/GraphTable.vue
@@ -69,25 +69,29 @@ export default {
   },
   methods: {
     fixSortingTable() {
-      const table = this.$refs.metricsTable
-      if (table.$el.children[0].children[0].children[0].children) {
-        const thead = table.$el.children[0].children[0].children[0].children
+      if (this.$refs.metricsTable) {
+        const table = this.$refs.metricsTable
+        if (table.$el.children[0].children[0].children[0].children) {
+          const thead = table.$el.children[0].children[0].children[0].children
 
-        if (thead[0].children && thead[1].children) {
-          const tr_0 = thead[0].children
-          const tr_1 = thead[1].children
-          if (tr_0) {
-            for (let i = 0; i <= 4; i++) {
-              if (tr_0[i].children[1]) {
-                tr_0[i].children[1].ariaLabel =
-                  this.$t("common.table.order_field") + tr_0[i].innerText
-                tr_0[i].children[1].role = "button"
-              }
-              if (tr_0[i].children[0]) {
-                tr_1[i].children[0].ariaLabel =
-                  this.$t("common.table.search_field") + tr_0[i].innerText
-                tr_1[i].children[0].title =
-                  this.$t("common.table.search_field") + tr_0[i].innerText
+          if (thead[0].children && thead[1].children) {
+            const tr_0 = thead[0].children
+            const tr_1 = thead[1].children
+            console.log(tr_0.length - 1)
+            const tr_len = tr_0.length - 1
+            if (tr_0) {
+              for (let i = 0; i <= tr_len; i++) {
+                if (tr_0[i].children[1]) {
+                  tr_0[i].children[1].ariaLabel =
+                    this.$t("common.table.order_field") + tr_0[i].innerText
+                  tr_0[i].children[1].role = "button"
+                }
+                if (tr_0[i].children[0]) {
+                  tr_1[i].children[0].ariaLabel =
+                    this.$t("common.table.search_field") + tr_0[i].innerText
+                  tr_1[i].children[0].title =
+                    this.$t("common.table.search_field") + tr_0[i].innerText
+                }
               }
             }
           }
@@ -95,67 +99,71 @@ export default {
       }
     },
     fixBodyTable() {
-      const table = this.$refs.metricsTable
-      const tBody = table.$el.children[0].children[0].children[1]
-      tBody.ariaLive = "polite"
-      if (tBody.children[0].children[0].children[0]) {
-        if (
-          tBody.children[0].children[0].children[0].className ==
-          "text-center my-5"
-        ) {
-          tBody.children[0].children[0].children[0].role = "alert"
-          console.log(tBody.children[0].children[0].children[0].role)
+      if (this.$refs.metricsTable) {
+        const table = this.$refs.metricsTable
+        const tBody = table.$el.children[0].children[0].children[1]
+        tBody.ariaLive = "polite"
+        if (tBody.children[0].children[0].children[0]) {
+          if (
+            tBody.children[0].children[0].children[0].className ==
+            "text-center my-5"
+          ) {
+            tBody.children[0].children[0].children[0].role = "alert"
+            console.log(tBody.children[0].children[0].children[0].role)
+          }
         }
       }
     },
     fixNavTable() {
-      const table = this.$refs.metricsTable
-      if (table.$el.children[1]) {
-        const nav = table.$el.children[1]
-        nav.ariaLabel = this.$t("common.table.pagination")
-        if (nav.children[0].children) {
-          let nav_buttons = nav.children[0].children
-          const next_button = nav_buttons.length - 2
-          const last_button = nav_buttons.length - 1
-          nav_buttons[0].children[0].ariaLabel = this.$t(
-            "common.table.go_to_first_page"
-          )
-          nav_buttons[0].children[0].title = this.$t(
-            "common.table.go_to_first_page"
-          )
-          nav_buttons[1].children[0].ariaLabel = this.$t(
-            "common.table.go_to_previous_page"
-          )
-          nav_buttons[1].children[0].title = this.$t(
-            "common.table.go_to_previous_page"
-          )
-          nav_buttons[next_button].children[0].ariaLabel = this.$t(
-            "common.table.go_to_next_page"
-          )
-          nav_buttons[next_button].children[0].title = this.$t(
-            "common.table.go_to_next_page"
-          )
-          nav_buttons[last_button].children[0].ariaLabel = this.$t(
-            "common.table.go_to_last_page"
-          )
-          nav_buttons[last_button].children[0].title = this.$t(
-            "common.table.go_to_last_page"
-          )
-          for (let i = 2; i <= nav_buttons.length - 3; i++) {
-            if (nav_buttons[i].className == "active page-item") {
-              nav_buttons[i].children[0].ariaLabel =
-                this.$t("common.table.current_page") +
-                nav_buttons[i].children[0].innerText
-              nav_buttons[i].children[0].title =
-                this.$t("common.table.current_page") +
-                nav_buttons[i].children[0].innerText
-            } else {
-              nav_buttons[i].children[0].ariaLabel =
-                this.$t("common.table.go_to_page") +
-                nav_buttons[i].children[0].innerText
-              nav_buttons[i].children[0].title =
-                this.$t("common.table.go_to_page") +
-                nav_buttons[i].children[0].innerText
+      if (this.$refs.metricsTable) {
+        const table = this.$refs.metricsTable
+        if (table.$el.children[1]) {
+          const nav = table.$el.children[1]
+          nav.ariaLabel = this.$t("common.table.pagination")
+          if (nav.children[0].children) {
+            let nav_buttons = nav.children[0].children
+            const next_button = nav_buttons.length - 2
+            const last_button = nav_buttons.length - 1
+            nav_buttons[0].children[0].ariaLabel = this.$t(
+              "common.table.go_to_first_page"
+            )
+            nav_buttons[0].children[0].title = this.$t(
+              "common.table.go_to_first_page"
+            )
+            nav_buttons[1].children[0].ariaLabel = this.$t(
+              "common.table.go_to_previous_page"
+            )
+            nav_buttons[1].children[0].title = this.$t(
+              "common.table.go_to_previous_page"
+            )
+            nav_buttons[next_button].children[0].ariaLabel = this.$t(
+              "common.table.go_to_next_page"
+            )
+            nav_buttons[next_button].children[0].title = this.$t(
+              "common.table.go_to_next_page"
+            )
+            nav_buttons[last_button].children[0].ariaLabel = this.$t(
+              "common.table.go_to_last_page"
+            )
+            nav_buttons[last_button].children[0].title = this.$t(
+              "common.table.go_to_last_page"
+            )
+            for (let i = 2; i <= nav_buttons.length - 3; i++) {
+              if (nav_buttons[i].className == "active page-item") {
+                nav_buttons[i].children[0].ariaLabel =
+                  this.$t("common.table.current_page") +
+                  nav_buttons[i].children[0].innerText
+                nav_buttons[i].children[0].title =
+                  this.$t("common.table.current_page") +
+                  nav_buttons[i].children[0].innerText
+              } else {
+                nav_buttons[i].children[0].ariaLabel =
+                  this.$t("common.table.go_to_page") +
+                  nav_buttons[i].children[0].innerText
+                nav_buttons[i].children[0].title =
+                  this.$t("common.table.go_to_page") +
+                  nav_buttons[i].children[0].innerText
+              }
             }
           }
         }
