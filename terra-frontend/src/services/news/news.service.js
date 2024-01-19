4e0ffa1836145cd187851cdde844818bffd8ecc9
mauroIstat <mbruno@istat.it>
2024-01-19 04:50:28 +0100

    Updated news section

--- terra-frontend/src/services/news/news.service.js
+++ terra-frontend/src/services/news/news.service.js
@@ -1,11 +1,14 @@
 import { axiosHack } from "@/http"
+import store from "@/store"
+
 export const newsService = {
   findAll
 }
 
 function findAll() {
+  const lan = store.getters["coreui/language"]
   return axiosHack
-    .get("/news")
+    .get("/news", { params: { lang: lan } })
     .then((res) => {
       var data = res.data ? res.data : {}
       //console.log(data);
