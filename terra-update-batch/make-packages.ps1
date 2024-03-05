30b626cc0efadf0139afaae555406cabf3aa28c5
Francesco Ortame <109461666+liminalfever@users.noreply.github.com>
2024-03-05 10:05:52 +0000

    update publish-batch-apps.yml

--- terra-update-batch/make-packages.ps1
+++ terra-update-batch/make-packages.ps1
@@ -2,6 +2,6 @@ $Version = ((Get-Content .\prepare-node.sh | ?{ $_ -match '^# Version [0-9\.]+$'
 Compress-Archive -Update -Path .\prepare-node.sh -DestinationPath .\prepare-node_$Version.zip
 
 $Version = ((Get-Content main.py | ?{ $_ -match '^# Version [0-9\.]+$' }) -split ' ')[2]
-Compress-Archive -Update -Path *.py,  modules, resources, execute, run.sh, requirements.txt -DestinationPath .\cosmoDataUpdate_$Version.zip
+Compress-Archive -Update -Path *.py, modules, resources, execute, run.sh, requirements.txt -DestinationPath .\terraDataUpdate_$Version.zip
 
 Write-Host "Operation completed."
